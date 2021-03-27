3b19fad41d7df0b3a001d1a8467a19b44577d0f6
MChung95 <markus_chung@hotmail.com>
2021-03-26 23:28:26 -0700

    Added JS code

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,20 @@
-// your JavaScript goes here, you can delete this comment
+let todoList = [
+    {todo: "clone repo for starter code", status: "complete"},
+    {todo: "change external links in index", status: "complete"},
+    {todo: "write JS code", status: "started"},
+    {todo: "push to github", status: "started"}
+];
+
+let completedTodos = []
+
+for(const obj of todoList) {
+    if (obj["status"] == "complete") {
+        completedTodos.push(obj);
+    }
+}
+
+console.log(completedTodos)
+
+for(let {todo, status = "Unknown"} of completedTodos) {
+    console.log(todo)
+    }
